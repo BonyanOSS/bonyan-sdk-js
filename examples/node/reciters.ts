af4b103408ff62e0ad5c06097196831f81c9a90f
Devon <167473386+arabpolice@users.noreply.github.com>
2026-05-18 00:42:34 +0000

    fix: fixed the reciters example

--- examples/node/reciters.ts
+++ examples/node/reciters.ts
@@ -2,8 +2,8 @@ import { BonyanClient } from '@bonyanoss/bonyan-api';
 
 const bonyan = new BonyanClient();
 
-const reciters = await bonyan.reciters.list();
+const { reciters } = await bonyan.reciters.list();
 console.log(reciters.slice(0, 5));
 
-const audio = await bonyan.reciters.getSurahAudio(1, 1);
+const audio = await bonyan.reciters.getSurah(1, 1);
 console.log(audio);
